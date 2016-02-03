deca955fcd09bf2d30d082b74e38719d4cb20f9e
arncard <arncard@hotmail.com>
2016-02-03 18:23:29 +0000

    update2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -46,7 +46,7 @@ class MoviesController < ApplicationController
     session[:ratings] = @ratings
 
     if @category and @sort
-      @movies = @movies.find(:all, :order => "#{@category} #{@sort}") 
+      @movies = @movies.find(:all) 
       session[:category] = @category
       session[:sort] = @sort
     end
